20f73e835ababf309ae94627ca783d3acb0a026c
Alfredo Eka Wibowo <alfredoeka@outlook.com>
2025-02-09 23:52:31 +0700

    feat: add input test case and assign list item test case

---
+++ tests/e2e/Object Repository/Page_TodoMVC Vue/todo_item--active.rs
@@ -0,0 +1,102 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>todo_item--active</name>
+   <tag></tag>
+   <elementGuidId>088e2f67-d30d-4000-9d5e-a4295c409961</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value>/html/body/section/main/ul/li/div[count(. | //*[@class = 'view']) = count(//*[@class = 'view'])]</value>
+      </entry>
+      <entry>
+         <key>CSS</key>
+         <value>div.view > label</value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>/html/body/section/main/ul/li[1]/div[1]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>BASIC</selectorMethod>
+   <smartLocatorCollection>
+      <entry>
+         <key>SMART_LOCATOR</key>
+         <value>internal:text=&quot;TODO 1&quot;i</value>
+      </entry>
+   </smartLocatorCollection>
+   <smartLocatorEnabled>false</smartLocatorEnabled>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>view</value>
+      <webElementGuid>17001d79-3fa8-49cd-bc1e-77ad0652279f</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>/html/body/section/main/ul/li/div</value>
+      <webElementGuid>e7603d68-1056-45bb-b62d-3fd25e4bf2ce</webElementGuid>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Toggle All Input'])[1]/following::label[1]</value>
+      <webElementGuid>433844f9-da91-4de2-adac-eb3a162c8a8c</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='todos'])[1]/following::label[2]</value>
+      <webElementGuid>147ee26d-8002-4044-b562-77de1173ba79</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Edit Todo Input'])[1]/preceding::label[1]</value>
+      <webElementGuid>599d1fde-8d67-4724-b1a9-1f9419517df1</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='TODO 2'])[1]/preceding::label[2]</value>
+      <webElementGuid>b8ad7244-db3e-4137-93b7-610cc86a021e</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>//*/text()[normalize-space(.)='TODO 1']/parent::*</value>
+      <webElementGuid>1dd36246-9ce9-4daa-8385-72e36cf01051</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//li/div/label</value>
+      <webElementGuid>e06eedd9-09fd-4e5a-89c3-25e312af5469</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:customAttributes</name>
+      <type>Main</type>
+      <value>//label[(text() = 'TODO 1' or . = 'TODO 1')]</value>
+      <webElementGuid>927c61c4-8dc6-43e4-ad3c-46d4c8a14c6a</webElementGuid>
+   </webElementXpaths>
+</WebElementEntity>
---
+++ tests/e2e/Object Repository/Page_TodoMVC Vue/todo_item--completed.rs
@@ -0,0 +1,90 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>todo_item--completed</name>
+   <tag></tag>
+   <elementGuidId>8ded403a-63a3-43fe-b66b-1465092c4408</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>CSS</key>
+         <value>div.view > label</value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>(.//*[normalize-space(text()) and normalize-space(.)='Toggle All Input'])[1]/following::label[1]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>BASIC</selectorMethod>
+   <smartLocatorCollection>
+      <entry>
+         <key>SMART_LOCATOR</key>
+         <value>internal:text=&quot;TODO 1&quot;i</value>
+      </entry>
+   </smartLocatorCollection>
+   <smartLocatorEnabled>false</smartLocatorEnabled>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>completed</value>
+      <webElementGuid>9b45351b-6c5c-42fb-8aa8-8bd9303fde04</webElementGuid>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Toggle All Input'])[1]/following::label[1]</value>
+      <webElementGuid>b381af8b-6157-46b1-aca3-dfb5cbc683ac</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='todos'])[1]/following::label[2]</value>
+      <webElementGuid>251e346c-5e4e-41f6-b3b6-719f2a290f6d</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Edit Todo Input'])[1]/preceding::label[1]</value>
+      <webElementGuid>a1ed1f52-22bd-4b10-9a0f-ad3b35851522</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='TODO 2'])[1]/preceding::label[2]</value>
+      <webElementGuid>38759abd-8dac-4ee7-aba8-045e6b18b88e</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>//*/text()[normalize-space(.)='TODO 1']/parent::*</value>
+      <webElementGuid>22b726e7-b210-4b62-9470-bc88cd864c3e</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//li/div/label</value>
+      <webElementGuid>fd2f6948-87d2-45d5-b0b7-84d8ef3a6b35</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:customAttributes</name>
+      <type>Main</type>
+      <value>//label[(text() = 'TODO 1' or . = 'TODO 1')]</value>
+      <webElementGuid>c0a47f08-2f4f-4a78-aaba-95ebac8cbef5</webElementGuid>
+   </webElementXpaths>
+</WebElementEntity>
